f4273596ba0c9bf3b186cfcd1b5856e751ba1d60
Furkan Vural <furkanvural52@gmail.com>
2024-01-10 16:29:05 +0300

    Create DevLog project

---
+++ DevLog/DevLog/DevLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevLogApp.swift
+//  DevLog
+//
+//  Created by furkan vural on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DevLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
