65a10fb30879c359194c5c062f24f6c6946601a0
Furkan Vural <furkanvural52@gmail.com>
2024-01-10 23:11:01 +0300

    Complete basic view

--- DevLog/DevLog/DevLogApp.swift
+++ DevLog/DevLog/DevLogApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct DevLogApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        Settings {
+            EmptyView()
         }
     }
 }
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    var window: NSWindow!
+
+    static private(set) var instance: AppDelegate!
+    
+    lazy var statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    let menu = ApplicationMenu()
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        AppDelegate.instance = self
+        statusBarItem.button?.image = NSImage(named: "DevLog")
+        statusBarItem.button?.imagePosition = .imageLeading
+        statusBarItem.menu = menu.createMenu()
+    }
+}
+
+
