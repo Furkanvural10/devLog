e2399d322aba18ec8b7496636e252d83a1c43420
Furkan Vural <furkanvural52@gmail.com>
2024-01-17 22:26:48 +0300

    Complete devlog view

--- DevLog/DevLog/PopoverTaskView.swift
+++ DevLog/DevLog/PopoverTaskView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct PopoverTaskView: View {
     
     @State var isSelected = false
-    @State private var selectedItem: Int?
+    @State private var selectedItem: Int = 0
     @State var addText = ""
     @State var itemList = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5","Item 1", "Item 2", "Item 3", "Item 4", "Item 5"]
     
@@ -29,9 +29,11 @@ struct PopoverTaskView: View {
                         .frame(width: 100, height: 35)
                         .cornerRadius(10)
                     HStack {
-                        Text("Features")
-                        Image(systemName: "list.star")
+                        
+                        Image(systemName: "circle.fill")
                             .foregroundStyle(.green)
+                        Text("Features")
+                            .foregroundStyle(selectedItem == 0 ? .white : .white.opacity(0.5))
                     }
                     .onTapGesture {
                         self.selectedItem = 0
@@ -44,9 +46,11 @@ struct PopoverTaskView: View {
                         .frame(width: 100, height: 35)
                         .cornerRadius(10)
                     HStack {
-                        Text("Bugs")
-                        Image(systemName: "exclamationmark.octagon.fill")
+                        
+                        Image(systemName: "circle.fill")
                             .foregroundStyle(.red)
+                        Text("Bugs")
+                            .foregroundStyle(selectedItem == 1 ? .white : .white.opacity(0.5))
                         
                     }
                 }
@@ -61,9 +65,10 @@ struct PopoverTaskView: View {
                         .frame(width: 100, height: 35)
                         .cornerRadius(10)
                     HStack {
-                        Text("Daily")
-                        Image(systemName: "circle.hexagongrid")
+                        Image(systemName: "circle.fill")
                             .foregroundStyle(.orange)
+                        Text("Daily")
+                            .foregroundStyle(selectedItem == 2 ? .white : .white.opacity(0.5))
                         
                     }
                 }
@@ -72,55 +77,60 @@ struct PopoverTaskView: View {
                     self.selectedItem = 2
                 }
             }
-            
-            HStack {
-                TextField("New Task", text: .constant("")) { text in
-                    print(addText)
-                }
-                .clipShape(RoundedRectangle(cornerRadius: 8))
-                .padding(.horizontal, 10)
-                
-                
-                
-                Button {
-                    print("Clicked")
-                    self.itemList.insert("New elemen", at: 0)
-                } label: {
-                    Text("Add")
-                }
-                .padding(.trailing, 10)
-                .foregroundStyle(.red)
-                .buttonStyle(.bordered)
-                
-            }
-            .padding(8)
-            List {
-                ForEach(itemList, id: \.self) { item in
-                    HStack {
-                        Toggle("", isOn: $isSelected)
-                            .toggleStyle(.automatic)
-                        ZStack {
-                            Rectangle()
-                                .frame(height: 40)
-                                .foregroundStyle(.gray.opacity(0.1))
-                                .clipShape(RoundedRectangle(cornerRadius: 5))
-                            Image(systemName: "multiply.circle")
-                                .foregroundColor(.white.opacity(0.3))
-                                .padding(4)
-                                .clipShape(Circle())
-                                .offset(x: 125, y: -17)
-                            Text(item)
-                        }
-                        Rectangle()
-                            .frame(width: 10, height: 10)
-                            .hidden()
-                    }
-                }
-            }
-            .scrollIndicators(.never)
-            
+            .background(.white.opacity(0.05))
+            .clipShape(RoundedRectangle(cornerRadius: 10))
         }
         
+        
+        HStack {
+            TextField("New Task", text: $addText) { text in
+                print(addText)
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .padding(.horizontal, 10)
+            
+            Button {
+                print("Clicked")
+                self.itemList.insert("New elemen", at: 0)
+            } label: {
+                Text("Add")
+            }
+            .padding(.trailing, 10)
+            .foregroundStyle(.red)
+            .buttonStyle(.bordered)
+            
+        }
+        .padding(8)
+        
+        
+        List {
+            ForEach(itemList, id: \.self) { item in
+                HStack {
+                    Toggle("", isOn: $isSelected)
+                        .toggleStyle(.automatic)
+                    ZStack {
+                        Rectangle()
+                            .frame(height: 40)
+                            .foregroundStyle(.gray.opacity(0.1))
+                            .clipShape(RoundedRectangle(cornerRadius: 5))
+                        Image(systemName: "multiply.circle")
+                            .foregroundColor(.white.opacity(0.3))
+                            .padding(4)
+                            .clipShape(Circle())
+                            .offset(x: 125, y: -17)
+                            .onTapGesture {
+                                print("Clicked text delete")
+                            }
+                        Text(item)
+                    }
+                    Rectangle()
+                        .frame(width: 5, height: 5)
+                        .hidden()
+                }
+            }
+        }
+        .scrollIndicators(.never)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
